005d1606cbac4e75bad5671822b421c0decc292d
arienandalibi <arienandalibi2@gmail.com>
2025-03-04 02:21:40 -0500

    Implemented tests for fusion_rrf. Test folder was added for all future tests and a CMake profile called flockmtl_tests was also added to run all tests. I've attempted to follow the src directory structure in the test folder. Catch2 is now a dependency in vcpkg.json

---
+++ test/include/flockmtl-test/functions/scalar/fusion_rrf/test_fusion_rrf.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "duckdb.hpp"
+#include "test_helpers.hpp"  // DuckDB's test helper functions, might use them eventually
+#include "catch.hpp"         // DuckDB uses Catch2 for tests so we'll do the same
