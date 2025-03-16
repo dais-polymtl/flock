f112ac192d4a3a6ecba3de8d540fcb70dcb46996
arienandalibi <arienandalibi2@gmail.com>
2025-03-16 06:23:23 -0400

    Updated documentation on RRF Fusion to indicate where rrf_constant = 60 originates from

--- src/include/flockmtl/functions/scalar/fusion_rrf.hpp
+++ src/include/flockmtl/functions/scalar/fusion_rrf.hpp
@@ -7,7 +7,7 @@ namespace flockmtl {
 /**
  * Performs Reciprocal Rank Fusion (RRF) to merge lists based multiple scoring sources,
  * as proposed by Cormack et al. in https://doi.org/10.1145/1571941.1572114.
- * They established that a rrf_constant of 60 was "near-optimal"
+ * They established that an rrf_constant of 60 was "near-optimal, but that the choice was not critical"
  */
 class FusionRRF : public ScalarFunctionBase {
 public:
