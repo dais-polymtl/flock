da324c33dc994e581f337c41f912048432e27c60
arienandalibi <arienandalibi2@gmail.com>
2025-03-16 06:13:31 -0400

    Added the sources for the papers which first proposed the different fusion algorithms implemented.

--- src/include/flockmtl/functions/scalar/fusion_combmnz.hpp
+++ src/include/flockmtl/functions/scalar/fusion_combmnz.hpp
@@ -4,6 +4,18 @@
 
 namespace flockmtl {
 
+/**
+ * Performs the CombMNZ algorithm to merge lists based multiple scoring sources,
+ * as proposed by Fox et al. Unfortunately, there is no DOI for this paper.
+ *
+ * Authors   = Edward A. Fox and Joseph A. Shaw
+ *
+ * Title     = Combination of Multiple Searches
+ *
+ * Publisher = National Institute of Standards and Technology (NIST)
+ *
+ * Year      = 1993
+ */
 class FusionCombMNZ : public ScalarFunctionBase {
 public:
     static void ValidateArguments(duckdb::DataChunk& args);
--- src/include/flockmtl/functions/scalar/fusion_rrf.hpp
+++ src/include/flockmtl/functions/scalar/fusion_rrf.hpp
@@ -4,9 +4,18 @@
 
 namespace flockmtl {
 
+/**
+ * Performs Reciprocal Rank Fusion (RRF) to merge lists based multiple scoring sources,
+ * as proposed by Cormack et al. in https://doi.org/10.1145/1571941.1572114
+ */
 class FusionRRF : public ScalarFunctionBase {
 public:
     static void ValidateArguments(duckdb::DataChunk& args);
+    /**
+     * Implements the RRF logic
+     * @param args Provided by core DuckDB when the function is executed in the CLI. Contains the arguments
+     * @return A vector of rankings for the entries, which will be processed by FusionRRF::Execute
+     */
     static std::vector<int> Operation(duckdb::DataChunk& args);
     static void Execute(duckdb::DataChunk& args, duckdb::ExpressionState& state, duckdb::Vector& result);
 };
