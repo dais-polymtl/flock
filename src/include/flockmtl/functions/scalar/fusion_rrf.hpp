5e6b797d036012a81079f7bedbe1966cc219c2e0
arienandalibi <arienandalibi2@gmail.com>
2025-03-16 06:13:31 -0400

    Added the sources for the papers which first proposed the different fusion algorithms implemented.

--- src/include/flockmtl/functions/scalar/fusion_rrf.hpp
+++ src/include/flockmtl/functions/scalar/fusion_rrf.hpp
@@ -4,9 +4,19 @@
 
 namespace flockmtl {
 
+/**
+ * Performs Reciprocal Rank Fusion (RRF) to merge lists based multiple scoring sources,
+ * as proposed by Cormack et al. in https://doi.org/10.1145/1571941.1572114.
+ * They established that a rrf_constant of 60 was "near-optimal"
+ */
 class FusionRRF : public ScalarFunctionBase {
 public:
     static void ValidateArguments(duckdb::DataChunk& args);
+    /**
+     * Implements the RRF logic
+     * @param args Provided by core DuckDB when the function is executed in the CLI. Contains the arguments
+     * @return A vector of rankings for the entries, which will be processed by FusionRRF::Execute
+     */
     static std::vector<int> Operation(duckdb::DataChunk& args);
     static void Execute(duckdb::DataChunk& args, duckdb::ExpressionState& state, duckdb::Vector& result);
 };
