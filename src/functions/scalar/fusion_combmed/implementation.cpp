ac8396ea71c8d1ad5752a5b2836b5519079eee0f
arienandalibi <arienandalibi2@gmail.com>
2025-03-27 05:39:05 -0400

    Updated FusionCombMED to properly keep track of NULL values and exclude them from normalization and median calculations.

--- src/functions/scalar/fusion_combmed/implementation.cpp
+++ src/functions/scalar/fusion_combmed/implementation.cpp
@@ -31,39 +31,52 @@ std::vector<std::string> FusionCombMED::Operation(duckdb::DataChunk& args, const
         cumulative_scores[i].first = i;
     }
 
-    // for each column (scoring system), we want a vector of individual input scores
+    // for each column (scoring system), we want a vector of individual input scores so we can normalize them
     for (int i = 0; i < num_different_scores; i++) {
-        // extract a single column's score values. Initializing this way ensures 0 for null values
+        // extract a single column's score values. We will keep track of which values were NULL and not extract them.
+        // This will allow normalizing and median calculations since extracted_scores has no NaN values in it.
+        // This means extracted_scores is NOT the same size as the input size since NaN values are removed.
+        // We will have to manually keep track of which were removed and where to maintain ordering.
         std::vector<double> extracted_scores(num_entries);
+        std::vector<bool> score_is_nan(num_entries);
+        int nan_count = 0;
         for (int j = 0; j < num_entries; j++) {
             auto valueWrapper = args.data[i].GetValue(j);
-            // null values are left as 0, treated as if the entry is not present in that scoring system's results
+            // null values are marked as NaN, we will ignore them during calculations later
             if (!valueWrapper.IsNull()) {
-                extracted_scores[j] = valueWrapper.GetValue<double>();
+                extracted_scores[j - nan_count] = valueWrapper.GetValue<double>();  // make sure all valid values follow
+                score_is_nan[j] = false;
             } else {
-                extracted_scores[j] = std::numeric_limits<double>::quiet_NaN();
+                score_is_nan[j] = true;
+                nan_count++;
             }
         }
+
+        // truncate the extracted scores in case we had NaN values and need to remove the trailing zeroes
+        extracted_scores.resize(num_entries - nan_count);
+
         // If all entries have the same score, then this scoring system can be considered useless and should be ignored
         if (std::adjacent_find(extracted_scores.begin(), extracted_scores.end(), std::not_equal_to<>()) == extracted_scores.end()) {
             continue;
         }
 
-        // we now normalize each scoring system independently, increasing hit counts appropriately
+        // we now normalize each scoring system independently
         extracted_scores = Normalizer::normalize(extracted_scores, normalization_method);
 
-        // add this column's scores to the scores for this entry
-        for (int k = 0; k < num_entries; k++) {
-            if (!std::isnan(extracted_scores[k])) {
-                cumulative_scores[k].second.push_back(extracted_scores[k]);
+        // add this column's scores to the scores for this entry while making sure to avoid NaN values
+        size_t score_index = 0;
+        for (size_t k = 0; k < num_entries; k++) {
+            if (!score_is_nan[k]) {
+                cumulative_scores[k].second.push_back(extracted_scores[score_index]);
+                score_index++;   // only increment the extracted scores index for non-NaN entries, separately from k
             }
         }
     }
 
-    // we will now calculate the median score for each entry
+    // Now that all scores are normalized and extracted, we can calculate the median score for each entry
     std::vector<std::pair<int, double>> median_scores(num_entries);
     for (int i = 0; i < num_entries; i++) {
-        median_scores[i].first = cumulative_scores[i].first;
+        median_scores[i].first = i;
         median_scores[i].second = FusionCombMED::calculateMedian(cumulative_scores[i].second);
     }
 
@@ -74,7 +87,7 @@ std::vector<std::string> FusionCombMED::Operation(duckdb::DataChunk& args, const
 
     // return the resulting ranking of all documents
     std::vector<std::string> results(num_entries);
-    for (int i = 0; i < num_entries; i++) {
+    for (size_t i = 0; i < num_entries; i++) {
         const int tmp_index = median_scores[i].first;
         // add 1 to rank because we want rankings to start at 1, not 0
         results[tmp_index] = std::to_string(i + 1) + " (" + std::to_string(median_scores[i].second) + ")";
